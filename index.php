aeb9c8351a670598d87f991e2c3023e27bc912c8
tobykalinux <92510360+tobykalinux@users.noreply.github.com>
2021-10-14 15:28:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </head>
     <body style="background:rgb(222, 222, 255)">
         <div style="text-align:center;padding:10% 0; font-family:arial">
-            <h1 style="font-size:80px; color:rgb(170, 170, 207)"><?php echo "Selamat Datang di"; ?></h1>
+            <h1 style="font-size:80px; color:rgb(170, 170, 207)"><?php echo "Toby Kalinux"; ?></h1>
             <h2 style="font-size: 20px; font-weight:300; margin-top:-50px; color:rgb(128, 128, 177)">
-                <i style="font-size:18px"><?php //echo $message; ?></i> &mdash; PT. Jouko Indonesia &mdash;
+                <i style="font-size:18px"><?php //echo $message; ?></i> &mdash; Git Coba Coba (GCC) &mdash;
             </h2>
         </div>
     </body>
