55f8e203ee968a2feb7fee10024742a55f0389c1
tobykalinux <92510360+tobykalinux@users.noreply.github.com>
2021-10-14 15:25:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-
 <?php
-header('location:http://portal.bnetfit.com');
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome</title>
+    </head>
+    <body style="background:rgb(222, 222, 255)">
+        <div style="text-align:center;padding:10% 0; font-family:arial">
+            <h1 style="font-size:80px; color:rgb(170, 170, 207)"><?php echo "Selamat Datang di"; ?></h1>
+            <h2 style="font-size: 20px; font-weight:300; margin-top:-50px; color:rgb(128, 128, 177)">
+                <i style="font-size:18px"><?php //echo $message; ?></i> &mdash; PT. Jouko Indonesia &mdash;
+            </h2>
+        </div>
+    </body>
+</html>
+
